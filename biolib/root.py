fed43b4465cb2549c2b0fba3b34a6b27af8f2fe4
laura-biolib <laura@biolib.com>
2022-04-20 08:28:16 +0000

    correct output

--- biolib/root.py
+++ biolib/root.py
@@ -163,7 +163,7 @@ with open("output/output.md", "w") as out:
     out.write(f"# AF-design {args.protocol} predicted sequences\n\n")
     out.write(f"**Target protein**: {args.pdb} **and chain** {args.chain}\n\n")
     out.write(f"**Binder length**: {args.binder_len}\n\n")
-    out.write(f"**Stages**: {args.stages}\n\n\n")   
+    out.write(f"**Design function**: {args.design}\n\n\n")   
     out.write(f"**Predicted binder sequences:**\n\n```\n")
     for seq in seqs:
         print("Predicted binder sequence:",seq)
